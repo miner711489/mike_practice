58c9b8535a6836a489ffc340ab7605e12edf5bfd
Mike <Lin>
2020-08-23 21:45:19 +0800

    test5

--- src/Main/MikeTest.java
+++ src/Main/MikeTest.java
@@ -12,11 +12,8 @@ public class MikeTest {
     public static void main(String[] args) {
 
         try {
-            //Mike Add Message
             ValidEmailFormat Valid = new ValidEmailFormat();
             System.out.println(Valid.Test());
-
-            System.out.println(Valid.Test() + "，123");
         } catch (Exception e) {
             System.out.println("Error Message:" + e.getMessage());
         }
