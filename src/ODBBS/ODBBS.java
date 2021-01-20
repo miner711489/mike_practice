b07e976940956b7b4da5cb65e755e3ef615be352
Mike <mike@mail.dsic.com.tw>
2021-01-20 23:36:45 +0800

    加入httpMethod.java 新增下載檔案功能

--- src/ODBBS/ODBBS.java
+++ src/ODBBS/ODBBS.java
@@ -1,6 +1,9 @@
 package ODBBS;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.util.Iterator;
 import java.util.List;
@@ -24,7 +27,7 @@ public class ODBBS {
     public void ODBBSMain() throws IOException, Exception {
 
         try {
-            String XmlString = doHttpGet();
+            String XmlString = doHttpGet2();
             parseXMLStr(XmlString);
         } catch (Exception e) {
             throw e;
@@ -32,14 +35,16 @@ public class ODBBS {
     }
 
     public void parseXMLStr(String XmlStr) throws Exception {
-        int index = 0;
-
+        int index = 1;
+        String desDir = "C:\\Users\\Mike\\Desktop\\OBBBS";
+        String Date = "1100120";
         Document document = DocumentHelper.parseText(XmlStr);
         Element rootElm = document.getRootElement();
         System.out.println("method1:");
         //方法一，使用elements()，轉成List(感覺這方法比較多人用)
         List<Element> eles = rootElm.elements();
         for (Element ele : eles) {
+            String SeqNO = Date + String.valueOf(index);
             if (ele.getName().toLowerCase().equals("senddate") || ele.getName().toLowerCase().equals("sendno")) {
                 continue;
             }
@@ -55,17 +60,25 @@ public class ODBBS {
             System.out.println("DiPortableUrl:" + (ele.selectSingleNode("DiPortableUrl") == null ? "Null" : ele.selectSingleNode("DiPortableUrl").getText()));
             System.out.println("DiName:" + (ele.selectSingleNode("DiName") == null ? "Null" : ele.selectSingleNode("DiName").getText()));
             System.out.println("DiUrl:" + (ele.selectSingleNode("DiUrl") == null ? "Null" : ele.selectSingleNode("DiUrl").getText()));
+            String di_pdf_url = ele.selectSingleNode("DiPortableUrl") == null ? "Null" : ele.selectSingleNode("DiPortableUrl").getText();
+            String di_pdf_name = ele.selectSingleNode("DiPortableName") == null ? "Null" : ele.selectSingleNode("DiPortableName").getText();
+            String di_url = ele.selectSingleNode("DiUrl") == null ? "Null" : ele.selectSingleNode("DiUrl").getText();
+            String di_name = ele.selectSingleNode("DiName") == null ? "Null" : ele.selectSingleNode("DiName").getText();
+            DownloadFile(desDir, SeqNO, di_pdf_url, di_pdf_name);
+            DownloadFile(desDir, SeqNO, di_url, di_name);
             if (ele.selectSingleNode("Attach") != null) {
                 List<Element> AttEles = ele.elements("Attach");
                 System.out.println("AttEles:" + AttEles.size());
                 for (Element attele : AttEles) {
-                    System.out.println("AttachName:" + (attele.selectSingleNode("AttachName") == null ? "Null" : attele.selectSingleNode("AttachName").getText()));
-                    System.out.println("AttachUrl:" + (attele.selectSingleNode("AttachUrl") == null ? "Null" : attele.selectSingleNode("AttachUrl").getText()));
+                    String AttachName = attele.selectSingleNode("AttachName") == null ? "Null" : attele.selectSingleNode("AttachName").getText();
+                    String AttachUrl = attele.selectSingleNode("AttachUrl") == null ? "Null" : attele.selectSingleNode("AttachUrl").getText();
+                    System.out.println("AttachName:" + AttachName);
+                    System.out.println("AttachUrl:" + AttachUrl);
+                    DownloadFile(desDir, SeqNO, AttachUrl, AttachName);
                 }
             } else {
                 System.out.println("AttEles:0");
             }
-
             index++;
         }
 
@@ -107,6 +120,24 @@ public class ODBBS {
         return rtnmsg;
     }
 
+    public String doHttpGet2() throws IOException, Exception {
+        String rtnmsg = "";
+        try {
+            CloseableHttpClient httpclient = HttpClients.createDefault();
+            String url = "https://www.odbbs.gov.tw/odbbs/html/getEDoc.jsp?senddate=";
+            String senddate = "20210120";
+            url += senddate;
+
+            httpMethod temp = new httpMethod();
+            rtnmsg = temp.doGet(url, null, null);
+
+        } catch (Exception e) {
+            throw e;
+        } finally {
+        }
+        return rtnmsg;
+    }
+
     public void getElementmethod2(Document document) {
         int index = 0;
         System.out.println("method2:");
@@ -122,6 +153,53 @@ public class ODBBS {
 
             index++;
         }
+    }
+
+    public void DownloadFile(String FilePath, String SeqNO, String Url, String FileName) throws IOException, Exception {
+
+        CloseableHttpClient httpclient = null;
+        httpclient = HttpClients.createDefault();
+        FileOutputStream fos = null;
+        InputStream is = null;
+        CloseableHttpResponse response = null;
+        try {
+            URI DataUrl = new URI(Url);
+            //建立HttpPost物件
+            HttpGet httpGet = new HttpGet(DataUrl);
+            //執行Post動作並取得回傳資料
+            response = httpclient.execute(httpGet);
+            File fFileUrl = new File(FilePath, SeqNO);
+            if (!fFileUrl.exists()) {
+                fFileUrl.mkdirs();
+            }
+            HttpEntity entity = response.getEntity();//取得回傳的資料
+            response.getStatusLine().getStatusCode();
+            response.getAllHeaders();
+            if (entity != null) {
+                is = entity.getContent();
+                fos = new FileOutputStream(new File(fFileUrl, FileName));
+                int inbyte;
+                while ((inbyte = is.read()) != -1) {
+                    fos.write(inbyte);
+                }
+            }
+            EntityUtils.consume(entity);//釋放資源
+            is.close();
+            fos.close();
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+            if (fos != null) {
+                fos.close();
+            }
+            if (response != null) {
+                response.close();
+            }
+        }
+
     }
 
 }
